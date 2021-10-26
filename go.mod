c2ca4206a4f3c04bc02cf4716680fef30c61cbf4
Matheus Melo <42951026+fox-two@users.noreply.github.com>
2021-10-26 13:33:29 -0300

    library for creating/checking the license

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Joinesty/shield-license
+
+go 1.17
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/hyperboloide/lk v0.0.0-20200504060759-b535f1973118
+)
---
+++ license.go
@@ -0,0 +1,107 @@
+package license
+
+import (
+	"encoding/json"
+	"fmt"
+	"regexp"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/hyperboloide/lk"
+)
+
+var getCurrentTime = time.Now
+
+type LicenseInfo struct {
+	Identifier  string
+	Username    string
+	CreatedDate time.Time
+	ValidDays   uint
+}
+
+func (l *LicenseInfo) IsExpired() bool {
+	expirationDate := l.CreatedDate.AddDate(0, 0, int(l.ValidDays))
+	return expirationDate.Before(getCurrentTime())
+}
+
+type CreateLicenseOpts struct {
+	Username   string
+	ValidDays  uint
+	PrivateKey string
+}
+
+var validUsernameRegex = regexp.MustCompile("^[a-zA-Z0-9 ]{1,32}$")
+
+var (
+	ErrInvalidUsername       = fmt.Errorf("username should be in the form \"[a-zA-Z0-9 ]{1,32}\"")
+	ErrInvalidUsernameLength = fmt.Errorf("username should have between 1 and 32 characters")
+)
+
+func CreateLicense(opts CreateLicenseOpts) (string, error) {
+	privateKey, err := lk.PrivateKeyFromB32String(opts.PrivateKey)
+	if err != nil {
+		return "", err
+	}
+
+	if len(opts.Username) <= 0 || len(opts.Username) > 32 {
+		return "", ErrInvalidUsernameLength
+	}
+
+	if !validUsernameRegex.MatchString(opts.Username) {
+		return "", ErrInvalidUsername
+	}
+
+	licenseInfo := LicenseInfo{
+		Identifier:  uuid.NewString(),
+		Username:    opts.Username,
+		CreatedDate: time.Now(),
+		ValidDays:   opts.ValidDays,
+	}
+
+	licenseDocBytes, err := json.Marshal(licenseInfo)
+	if err != nil {
+		return "", err
+	}
+
+	license, err := lk.NewLicense(privateKey, licenseDocBytes)
+	if err != nil {
+		return "", err
+	}
+
+	licenseString, err := license.ToB32String()
+	if err != nil {
+		return "", err
+	}
+
+	return licenseString, nil
+}
+
+type CheckLicenseOpts struct {
+	License   string
+	PublicKey string
+}
+
+func CheckLicense(opts CheckLicenseOpts) (*LicenseInfo, error) {
+	publicKey, err := lk.PublicKeyFromB32String(opts.PublicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	license, err := lk.LicenseFromB32String(opts.License)
+	if err != nil {
+		return nil, err
+	}
+
+	if ok, err := license.Verify(publicKey); err != nil {
+		return nil, err
+	} else if !ok {
+		return nil, fmt.Errorf("invalid signature")
+	}
+
+	var licenseInfo LicenseInfo
+	if err := json.Unmarshal(license.Data, &licenseInfo); err != nil {
+		return nil, err
+	}
+
+	return &licenseInfo, nil
+}
